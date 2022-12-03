2d5e3697ee4a62685a8c7f54962d68a2de90d358
toystix1992 <toystix1992@gmail.com>
2022-12-03 21:57:36 +0300

    feat:  add solution for module part of the task

---
+++ src/modules/esm.mjs
@@ -0,0 +1,39 @@
+import {sep} from 'path';
+import { release, version } from 'os';
+import * as http from 'http';
+import {c} from './files/c.js';
+import a from './files/a.json' assert { type: 'json'};
+import b from './files/b.json' assert { type: 'json' };
+import * as url from 'url';
+const __filename = url.fileURLToPath(import.meta.url);
+const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
+console.log(a,b);
+
+const random = Math.random();
+
+let unknownObject;
+
+random > 0.5?  unknownObject = a: unknownObject = b;
+
+
+console.log(`Release ${release()}`);
+console.log(`Version ${version()}`);
+console.log(`Path segment separator is "${sep}"`);
+
+console.log(`Path to current file is ${__filename}`);
+console.log(`Path to current directory is ${__dirname}`);
+
+const myServer = http.createServer((_, res) => {
+    res.end('Request accepted');
+});
+
+const PORT = 3000;
+
+console.log(unknownObject);
+
+myServer.listen(PORT, () => {
+    console.log(`Server is listening on port ${PORT}`);
+    console.log('To terminate it, use Ctrl+C combination');
+});
+
+export {unknownObject, myServer};
