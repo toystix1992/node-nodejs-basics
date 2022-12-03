df8a98298403ce7e43723c42bd9e08c7966b8033
toystix1992 <toystix1992@gmail.com>
2022-12-04 00:57:30 +0300

    feat: add solution for the Command line interface(src/cli) task

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,26 @@
-const parseArgs = () => {
-    // Write your code here 
+import { argv } from 'node:process';
+const mergeKeyValue = (arr) => {
+    let counter = 0;
+    let solutionString = '';
+    arr.forEach(element => {
+        counter++;
+        if (counter === 1) {
+            solutionString += ` ${element} is `;
+        } else if (counter === 2) {
+            counter = 0
+            solutionString += `${element},`;
+        };
+    });
+    return solutionString;
+}
+
+export const parseArgs = () => {
+    const args = argv.slice(2);
+    const cleanArgs = args.map(arg => {
+        const stringCutter = arg.includes('--') ? 2 : 0;
+        return arg.slice(stringCutter)
+    })
+    console.log(mergeKeyValue(cleanArgs));
 };
 
 parseArgs();
--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,10 @@
+import { env } from 'node:process';
 const parseEnv = () => {
-    // Write your code here 
+    const filteresVars = Object.entries(env)
+        .filter(vari => vari[0].includes("RSS_"));
+    filteresVars.forEach(vari => {
+        console.log(`${vari[0]} = ${vari[1]}`);
+    });
 };
 
 parseEnv();
