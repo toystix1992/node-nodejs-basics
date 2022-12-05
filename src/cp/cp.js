01cbc0b4a2c6e70e450479f036a35b74069e6aef
toystix1992 <toystix1992@gmail.com>
2022-12-05 22:56:37 +0300

    feat: add solution for cp task

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,5 +1,12 @@
-const spawnChildProcess = async (args) => {
-    // Write your code here
+import { stdin } from 'process';
+import { spawn } from 'child_process';
+const filePath = './src/cp/files/script.js'
+
+export const spawnChildProcess = async (args) => {
+    const childProcess = spawn('node', [filePath, ...args]);
+
+    childProcess.stdout.pipe(process.stdout);
+    stdin.pipe(childProcess.stdin);
 };
 
-spawnChildProcess();
+spawnChildProcess(['arg1', 'arg2']);
